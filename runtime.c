a8b6ce3381e7942d3144edd52e0231317ab52b81
zouyonghao <yonghaoz1994@gmail.com>
2024-03-19 00:48:02 +0100

    remove gcj stub

--- runtime.c
+++ runtime.c
@@ -5,24 +5,6 @@ void Kernel_writeMemory_6270829245fa537989e2e2b8ddb7075d4c724a03(
   *((uint16_t *)addr) = (uint8_t)_byte;
 }
 
-// void main()
-// {
-// 	_ZN10HelloWorld4mainEJvP6JArrayIPN4java4lang6StringEE();
-// }
-
-/* runtime functions in libgcj */
-void _ZN4java4lang6ObjectC1Ev() {}
-void _Jv_InitClass() {}
-void _Jv_RegisterResource() {}
-void _ZN4java4lang6Object8finalizeEJvv() {}
-void _ZN4java4lang6Object8hashCodeEJiv() {}
-void _ZN4java4lang6Object6equalsEJbPS1_() {}
-void _ZN4java4lang6Object8toStringEJPNS0_6StringEv() {}
-void _ZN4java4lang6Object5cloneEJPS1_v() {}
-void _ZN4java4lang6Object22throwNoSuchMethodErrorEJvv() {}
-void _ZTVN4java4lang5ClassE() {}
-void _ZN4java4lang6Object6class$E() {}
-
 void Safepoint_enterSlowPathSafepointCheck_c39040d70cf6aa8104bda7eae589ec9601192610() {
 }
 
